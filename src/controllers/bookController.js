d54a5e554c9ee4a71b71de3006ff1fe9e35fa32f
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2022-05-18 16:26:03 +0700

    update : model book , distributor  and  Controller  book , distributo

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,10 +1,9 @@
 const { firestore, db } = require("../../db/db");
-const distributorModel = require("../models/distributor");
+const bookModel = require("../models/books");
 
 exports.getAll = async (req, res, next) => {
   try {
-    const distributorId = req.userId;
-    const getAll = await distributorModel.getBookAll(distributorId);
+    const getAll = await bookModel.getBookAll();
     const books = getAll.docs.map((doc) => doc.data());
     res.status(201).json({
       status: 200,
@@ -17,84 +16,22 @@ exports.getAll = async (req, res, next) => {
     next(error);
   }
 };
-exports.create = async (req, res, next) => {
-  try {
-    const distributorId = req.userId;
-    const bookTitle = req.body.bookTitle;
-    const author = req.body.author;
-    const Category = req.body.Category;
-    const description = req.body.description;
-    const price = req.body.price;
-    const fileUrl = req.body.fileUrl;
-    data = {};
-    data.bookTitle = bookTitle;
-    data.author = author;
-    data.Category = Category;
-    data.description = description;
-    data.price = price;
-    data.release = false;
-    data.createAt = firestore.FieldValue.serverTimestamp();
-    data.fileUrl = "https://www.googleapis.com/books/";
-    const addBook = await distributorModel.createBook(distributorId, data);
-    res.status(201).json({
-      status: 200,
-      newBook: data,
-    });
-  } catch (error) {
-    if (!error.statusCode) {
-      error.statusCode = 404;
-    }
-    next(error);
-  }
-};
 
 exports.getById = async (req, res, next) => {
   try {
-    const distributorId = req.userId;
+    const userId = req.userId;
     const bookId = req.params.bookId;
-    const book = await distributorModel.getBookById(distributorId, bookId);
-    res.status(201).json({
-      status: 200,
-      BookDetails: book,
-    });
-  } catch (error) {
-    if (!error.statusCode) {
-      error.statusCode = 404;
+    const book = await bookModel.getBookById(bookId);
+    if (!book) {
+      res.status(404).json({ status: 404, msg: "Don't have any book" });
+    } else if (book.release === false) {
+      res.status(404).json({ status: 404, msg: "Don't have any book" });
+    } else {
+      res.status(201).json({
+        status: 200,
+        BookDetails: book,
+      });
     }
-    next(error);
-  }
-};
-
-exports.update = async (req, res, next) => {
-  try {
-    const distributorId = req.userId;
-    const bookId = req.params.bookId;
-    const book = await distributorModel.updateBook(
-      distributorId,
-      bookId,
-      req.body
-    );
-    res.status(201).json({
-      status: 200,
-      BookDetails: book,
-    });
-  } catch (error) {
-    if (!error.statusCode) {
-      error.statusCode = 404;
-    }
-    next(error);
-  }
-};
-
-exports.delete = async (req, res, next) => {
-  try {
-    const distributorId = req.userId;
-    const bookId = req.params.bookId;
-    const book = await distributorModel.deleteBook(distributorId, bookId);
-    res.status(201).json({
-      status: 200,
-      msg: "Delete successful",
-    });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 404;
