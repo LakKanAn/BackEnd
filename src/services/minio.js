9c2398f53f8ffe1a540db24687075f7f5eb61fb9
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2022-08-29 22:06:52 +0700

    change: minio local -> server

--- src/services/minio.js
+++ src/services/minio.js
@@ -1,11 +1,11 @@
 const Minio = require("minio");
 
 const minioClient = new Minio.Client({
-  // endPoint: process.env.ENDPOINT,
-  endPoint: "localhost",
-  port: 9000,
-  // useSSL: true,
-  useSSL: false,
+  endPoint: process.env.ENDPOINT,
+  // endPoint: "localhost",
+  // port: 9000,
+  useSSL: true,
+  // useSSL: false,
   accessKey: process.env.ACCESSKEY,
   secretKey: process.env.SECRETKEY,
 });
