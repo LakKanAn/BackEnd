b71d5d543cc37ca6df644255ec63b294da2dd2c5
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2022-11-13 20:48:21 +0700

    update : refactor code in multer

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const uploadBook = multer({
-  limits: { fieldSize: process.env.MAX_FILE_SIZE },
+  limits: { fieldSize: process.env.MAX_FILE_SIZE || 1024 * 1024 * 8 },
   fileFilter(file, cb) {
     if (!file.originalname.match(/\.(png|jpg|jpeg|pdf)$/)) {
       return cb(new Error("Please upload book or content"));
