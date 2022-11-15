f004eed0178981b6948d04a249fc3a88debd3375
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2022-11-15 10:23:08 +0700

    fix : add distributor

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,11 +6,11 @@ const reportModel = require("../models/reports");
 const transactionModel = require("../models/transactions");
 const validator = require("email-validator");
 const adminUid = process.env.ADMIN_UID;
-
+const adminUid_prd = process.env.ADMIN_UID_PRD;
 exports.addDistributor = async (req, res, next) => {
   try {
     const userId = req.userId;
-    if (userId != adminUid) {
+    if (userId != adminUid || userId != adminUid_prd) {
       return res.status(403).json({ status: 403, msg: "not a permission" });
     }
     const email = req.body.email;
