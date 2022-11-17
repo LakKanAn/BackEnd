c893ac33312a2c229ea43c1c43fa5d2e86d775a7
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2022-11-17 11:51:24 +0700

    fix : check file

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,12 +1,20 @@
 const multer = require("multer");
 
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|pdf/;
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype) {
+    return cb(null, true);
+  } else {
+    cb("error: Please upload book or content");
+  }
+}
+
 const uploadBook = multer({
   limits: { fileSize: process.env.MAX_FILE_SIZE || 1024 * 1024 * 8 },
-  fileFilter(file, cb) {
-    if (!file.originalname.match(/\.(png|jpg|jpeg|pdf)$/)) {
-      return cb(new Error("Please upload book or content"));
-    }
-    cb(null, true);
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
   },
 });
 
